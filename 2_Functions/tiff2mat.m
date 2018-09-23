adb9327f7c7aeb6c4c725df8ac2560fd9ccd75a6
Issac Loo <issacjaloo@gmail.com>
2018-09-23 19:20:36 -0400

    tiff2mat function

---
+++ 2_Functions/tiff2mat.m
@@ -0,0 +1,23 @@
+function [imageArray] = tiff2mat(file_name)
+%TIFF2MAT converts tiff img file to 3d matrix
+% Specify the folder where the files live.
+myFolder = strcat('/Users/Issac/Documents/GitHub/1_Data/Exp 2/ACT data/',file_name);
+% Check to make sure that folder actually exists.  Warn user if it doesn't.
+if ~isdir(myFolder)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+  uiwait(warndlg(errorMessage));
+  return;
+end
+% Get a list of all files in the folder with the desired file name pattern.
+filePattern = fullfile(myFolder, '*.tiff');
+theFiles = dir(filePattern);
+test = imread(strcat(myFolder,'/0001.tiff'));
+imageArray = zeros(size(test,1),size(test,2),length(theFiles));
+for k = 1 : length(theFiles)
+  baseFileName = theFiles(k).name;
+  fullFileName = fullfile(myFolder, baseFileName);
+  imageArray(:,:,k) = imread(fullFileName);
+end
+
+end
+
