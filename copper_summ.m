a94ae60bdb39ba2bd5c6411938bb0b5392de846a
Issac Loo <issacjaloo@gmail.com>
2018-10-19 09:56:12 -0400

    remove folder 4

--- copper_summ.m
+++ copper_summ.m
@@ -3,14 +3,17 @@ T1 = tiff2mat(file_name1);
 file_name2 = input('File name: ','s');
 T1a = tiff2mat(file_name2);
 clear file_name1 file_name2
-
 T1a = imresize3(T1a, size(T1));
+T1(T1 == 0) = NaN;
+T1a(T1a == 0) = NaN;
 
 %%
-T1a_t = T1a(:,:,1);
-T1_t = T1(:,:,1);
-T1a_t(T1a_t < 39000) = 0;
-%T1_t(T1_t < 30000) = 0;
-imshow(T1a_t,[])
+%T1_t = T1;
+%T1_t(T1_t > 52000 | T1_t < 32000) = NaN;
+T1_t = histeq(T1(:,:,2),100);
+imshowpair(T1(:,:,2),T1_t,'montage')
+
+%imshow3D(T1,[])
 %figure(2)
-%imshow(T1_t,[])
+%imshow3D(T1a,[])
+%imshow3D(T1_t,[])
