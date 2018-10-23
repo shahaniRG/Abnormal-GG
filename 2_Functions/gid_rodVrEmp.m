ef7d0943f281bf2cb11745fea1b49cff6c0e70d2
Issac Loo <issacjaloo@gmail.com>
2018-10-23 10:44:38 -0400

    Added new Gid_RodV

--- 2_Functions/gid_rodVrEmp.m
+++ 2_Functions/gid_rodVrEmp.m
@@ -1,4 +1,4 @@
-function [RodV, coords] = gid_rodV(h5file, unique_gid, gid_map)
+function [RodV, coords] = gid_rodVrEmp(h5file, unique_gid, gid_map)
 %gid_rodV gives a single rodrigues vector that respresents each grain
 %   Input: h5file, unique_gid, gid_map, comp
 %   Output: RodV, coords, comp_mean
@@ -12,5 +12,6 @@ for i = unique_gid'
     RodV(i,2) = rodV(2,coords(i,1),coords(i,2),coords(i,3));
     RodV(i,3) = rodV(3,coords(i,1),coords(i,2),coords(i,3));
 end
+coords = coords(any(coords,2),:);
+RodV = RodV(any(RodV,2),:);
 end
-
